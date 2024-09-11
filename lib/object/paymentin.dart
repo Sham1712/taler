4233e59c41010acc265583b343d8420d3ad028d6
soorya <sooryasivask@gmail.com>
2024-09-11 23:11:49 +0530

    ledger changes

--- lib/object/paymentin.dart
+++ lib/object/paymentin.dart
@@ -1,29 +1,31 @@
 // ignore_for_file: camel_case_types
 
-
 import 'customer.dart';
 
 const String col_invoiceid = 'invoiceid';
 const String col_customerid = 'customerid';
 const String col_amount = 'amount';
+const String col_isin = 'isin';
 const String col_date = 'date';
 const String col_mode = 'mode';
 const String col_bank = 'bank';
 const String col_billadjamount = 'billadjamount';
 const String col_notes = 'notes';
 
-class PaymentIn{
+class Payment{
 
   double amount, billadjamount;
   DateTime date;
+  bool isin;
   String invoiceid, customerid, mode, bank, notes;
   String? id;
 
-  PaymentIn({
+  Payment({
     this.id,
     required this.invoiceid,
     required this.customerid,
     required this.amount,
+    required this.isin,
     required this.date,
     required this.mode,
     required this.bank,
@@ -31,11 +33,12 @@ class PaymentIn{
     required this.notes,
   });
 
-  factory PaymentIn.fromMap(Map<String, dynamic> map) =>  PaymentIn(
+  factory Payment.fromMap(Map<String, dynamic> map) =>  Payment(
     id : map[col_id],
     invoiceid : map[col_invoiceid],
     customerid : map[col_customerid],
     amount : map[col_amount],
+    isin : map[col_isin] ,
     date : map[col_date],
     mode : map[col_mode],
     bank : map[col_bank],
@@ -50,6 +53,7 @@ class PaymentIn{
       col_invoiceid: invoiceid,
       col_customerid: customerid,
       col_amount: amount,
+      col_isin: isin,
       col_date: date,
       col_mode: mode,
       col_bank: bank,
